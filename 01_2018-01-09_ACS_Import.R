d926466fe199f95d08a73ce22edb1b6b77f61dad
Cristian E. Nuno <nuno.e.cristian@gmail.com>
2018-01-24 09:14:23 -0600

    Created a master CT to CCA crosswalk
    Also transformed the acs objects into data frames by cbinding the 'geography' and 'estimate' data frames.

--- 01_2018-01-09_ACS_Import.R
+++ 01_2018-01-09_ACS_Import.R
@@ -1,14 +1,107 @@
 ########################
 # Title : ACS_Import
-# Purpose : Import census data and select indicators
+# Purpose : Updating Census Data - Selected socioeconomic indicators in Chicago, 2008 - 2012
+#             Using ACS 2012-2016 5-Year Estimate data
 # Author: Patricia van Hissenhoven 
 ########################
 
-# import necessary packages
+## load necessary packages
 library( acs )
+library( dplyr )
 library( sp )
 library( rgdal )
-library( stringr )
+
+## load necessary data
+
+# transform URL character vector into spatial dataframe
+comarea606 <- readOGR( dsn = "https://data.cityofchicago.org/api/geospatial/cauq-8yn6?method=export&format=GeoJSON"
+                       , layer = "OGRGeoJSON"
+                       , stringsAsFactors = FALSE
+                       , verbose = FALSE # to hide progress message after object is created
+)
+
+
+
+#2010 Census Tracts - Source: https://www.cityofchicago.org/city/en/depts/doit/dataset/boundaries_-_censustracts.html
+chicago.census.tracts <- readOGR( dsn = "https://data.cityofchicago.org/api/geospatial/5jrd-6zik?method=export&format=GeoJSON"
+                                  , layer = "OGRGeoJSON"
+                                  , stringsAsFactors = FALSE
+)
+
+# What's inside the data slot?
+str( object = slot( object = chicago.census.tracts, name = "data" ) )
+
+# 'data.frame':	801 obs. of  9 variables:
+# $ statefp10 : chr  "17" "17" "17" "17" ...
+# $ name10    : chr  "8424" "8403" "8411" "8412" ...
+# $ commarea_n: chr  "44" "59" "34" "31" ...
+# $ namelsad10: chr  "Census Tract 8424" "Census Tract 8403" "Census Tract 8411" "Census Tract 8412" ...
+# $ commarea  : chr  "44" "59" "34" "31" ...
+# $ geoid10   : chr  "17031842400" "17031840300" "17031841100" "17031841200" ...
+# $ notes     : chr  "" "" "" "" ...
+# $ tractce10 : chr  "842400" "840300" "841100" "841200" ...
+# $ countyfp10: chr  "031" "031" "031" "031" ...
+
+# Excellent. They've given us a crosswalk between fips codes (statefp10+name10+tractce10)
+# and commarea!
+
+# NOTE: There a few judgements being made regarding which census tracts belong in which community areas
+View( x = slot( object = chicago.census.tracts, name = "data")[ which(
+  slot( object = chicago.census.tracts, name = "data")$note != ""
+) , ])
+
+# store transparent gray color
+custom.color <- rgb( r = 204, g = 204, b = 204, alpha = 100, maxColorValue = 255 )
+
+# overlay the two geographies 
+# on top of each other to gain 
+# a better sense of the judgements
+# being made
+par( mar = c(2, 0, 2, 0 ) )
+plot( x = chicago.census.tracts
+      , col = "#4d4d4d"
+      , border = "#4d4d4d"
+      , main = "Overlaying Geographies in Chicago"
+      )
+plot( x = comarea606
+      , col = custom.color
+      , border = custom.color
+      , add = TRUE
+      )
+legend( x = "left"
+        , legend = c( "Chicago Census Tracts", "Chicago Current Community Areas" )
+        , pch = 15
+        , pt.cex = 2
+        , col = c( "#4d4d4d", "#CCCCCC" )
+        , bty = "n"
+        , title = "Legend"
+        )
+mtext( side = 1
+       , adj = 0.99
+       , cex = 0.75
+       , line = 1
+       , text = "Source: Chicago Data Portal"
+       )
+
+# Because the goal is to update this data set,
+# it is an assumption we are making that the
+# City used the crosswalk contain in the census tract spatial polygons data frame
+# to determine which census tracts belong in which community areas
+
+# create the census tract to community area number crosswalk
+census.tract.community.area.crosswalk.df <-
+  slot( object = chicago.census.tracts, name = "data" )[ c( "commarea_n", "tractce10" ) ]
+
+# create the community area number to community area name crosswalk
+community.area.number.name.crosswalk.df <- 
+  slot( object = comarea606, name = "data" )[ c( "community", "area_numbe" ) ]
+
+# create a master census tract to community area crosswalk
+master.census.tract.community.area.crosswalk.df <-
+  left_join( x = census.tract.community.area.crosswalk.df
+             , y = community.area.number.name.crosswalk.df
+             , by = c( "commarea_n" = "area_numbe" ) 
+             )
 
 # my personal acs api key
 my.key <- "6b2a3bf0f9ec6f097062213125bc40cad0351578"
@@ -21,7 +114,12 @@ cook.county.census.tracts <- geo.make(state = "IL"
 
 # Download ACS 2011-2015 5-Year Estimate Data
 # https://census.gov/programs-surveys/acs/technical-documentation/table-shells.html
-# Avoiding the use of the `table.number` parameter
+# Methodology
+# 1. Avoiding the use of the `table.number` parameter
+# 2. keep the 'geography' and 'estimate' data frames within each acs object
+# 3. keep only the census tracts from the acs data frame
+#    that appear in the master.census.tract.community.area.crosswalk.df
+#   
 
 # population by age group by gender
 population.df <- acs.fetch( endyear = 2016
@@ -38,6 +136,11 @@ population.df <- acs.fetch( endyear = 2016
                                                    )
                                    , key = my.key 
                             )
+population.df <- cbind.data.frame(
+  slot( object = population.df, name = "geography" )
+  , slot( object = population.df, name = "estimate" )
+)
+
 # this one takes about ~1 minute
 # educational attainment
 education.df <- acs.fetch( endyear = 2016
@@ -53,6 +156,10 @@ education.df <- acs.fetch( endyear = 2016
                                                      )
                                             )
                             , key = my.key 
+)
+education.df <- cbind.data.frame(
+  slot( object = education.df, name = "geography" )
+  , slot( object = education.df, name = "estimate" )
 )
 
 # poverty status
@@ -71,6 +178,10 @@ poverty.df <- acs.fetch( endyear = 2016
                              )
                            , key = my.key 
                            )
+poverty.df <- cbind.data.frame(
+  slot( object = education.df, name = "geography" )
+  , slot( object = education.df, name = "estimate" )
+)
 
 # per capita income
 # in 2016-inflation adjusted dollars
@@ -79,6 +190,10 @@ pci.df <- acs.fetch( endyear = 2016
                          , geography = cook.county.census.tracts
                          , variable = "B19301_001"
                          , key = my.key 
+)
+pci.df <- cbind.data.frame(
+  slot( object = education.df, name = "geography" )
+  , slot( object = education.df, name = "estimate" )
 )
 
 # employment status for folks aged 16+ by sex by age
@@ -97,6 +212,10 @@ employment.df <- acs.fetch( endyear = 2016
                             )
                             , key = my.key
                             )
+employment.df <- cbind.data.frame(
+  slot( object = education.df, name = "geography" )
+  , slot( object = education.df, name = "estimate" )
+)
 
 # tenure by occupants per room
 occupants.per.room.df <- acs.fetch( endyear = 2016
@@ -114,12 +233,19 @@ occupants.per.room.df <- acs.fetch( endyear = 2016
                                     )
                                     , key = my.key
 )
+occupants.df <- cbind.data.frame(
+  slot( object = education.df, name = "geography" )
+  , slot( object = education.df, name = "estimate" )
+)
 
-# filter data to match the socioeconomic descriptors
+## filter data to match the socioeconomic descriptors
 
 # Name:           Percent Aged Under 18 or Over 64	
 # Description:    Percent of the population under 18 or over 64 years of age 
 #                 (i.e., dependency)
+
+
+
 
 # Name:           Percent Aged 25+ Without High School Diploma
 # Description:    Percent of persons aged 25 years or older without a high school diploma
@@ -149,61 +275,19 @@ occupants.per.room.df <- acs.fetch( endyear = 2016
 #                 The scores are standardized according to the data for the 77 community areas, 
 #                 and therefore cannot be compared to scores generated for other jurisdictions.
 
-# aggregation - not complete #
+## aggregation - not complete #
+# Please use dplyr for aggregation
+# and see here for more info: http://www.datacarpentry.org/R-genomics/04-dplyr.html#split-apply-combine_data_analysis_and_the_summarize()_function
 
-# store Chicago current community area
-# GeoJSON URL as a character vector
-geojson_comarea_url <- "https://data.cityofchicago.org/api/geospatial/cauq-8yn6?method=export&format=GeoJSON"
+## Create a data frame that contains 78 rows and 9 columns
+## 77 rows for each community area, plus 1 for the entire City
+## 2 columns for the community area number + name
+## 6 columns for each socioeconomic indicator
+## 1 column for the Hardship Index (excluding a value for the entire City )
+## See here for an example of the schema: https://data.cityofchicago.org/Health-Human-Services/Census-Data-Selected-socioeconomic-indicators-in-C/kn9c-c2s2/data
 
-# transform URL character vector into spatial dataframe
-comarea606 <- readOGR( dsn = geojson_comarea_url
-                       , layer = "OGRGeoJSON"
-                       , stringsAsFactors = FALSE
-                       , verbose = FALSE # to hide progress message after object is created
-)
+## Export the results into the /Data folder as both an RDS and CSV
 
-
-
-#2010 Census Tracts - Source: https://www.cityofchicago.org/city/en/depts/doit/dataset/boundaries_-_censustracts.html
-chicago.census.tracts <- readOGR( "https://data.cityofchicago.org/api/geospatial/5jrd-6zik?method=export&format=GeoJSON"
-                                  , "OGRGeoJSON"
-                                  , stringsAsFactors = FALSE
-)
-
-
-# What's inside the data slot?
-str( object = slot( object = chicago.census.tracts, name = "data" ) )
-
-# 'data.frame':	801 obs. of  9 variables:
-# $ statefp10 : chr  "17" "17" "17" "17" ...
-# $ name10    : chr  "8424" "8403" "8411" "8412" ...
-# $ commarea_n: chr  "44" "59" "34" "31" ...
-# $ namelsad10: chr  "Census Tract 8424" "Census Tract 8403" "Census Tract 8411" "Census Tract 8412" ...
-# $ commarea  : chr  "44" "59" "34" "31" ...
-# $ geoid10   : chr  "17031842400" "17031840300" "17031841100" "17031841200" ...
-# $ notes     : chr  "" "" "" "" ...
-# $ tractce10 : chr  "842400" "840300" "841100" "841200" ...
-# $ countyfp10: chr  "031" "031" "031" "031" ...
-
-# Excellent. They've given us a crosswalk between fips codes (statefp10+name10+tractce10)
-# and commarea!
-
-# NOTE: There a few judgements being made regarding which census tracts belong in which community areas
-View( x = slot( object = chicago.census.tracts, name = "data")[ which(
-  slot( object = chicago.census.tracts, name = "data")$note != ""
-) , ])
-
-# overlay to spdf on top of each other
-plot( x = chicago.census.tracts, col = "#4d4d4d", border = "#4d4d4d")
-plot( x = comarea606, col = "#CCCCCC", border = "#CCCCCC", add = TRUE)
-
-##############
-# I will create geo.make to create user-specified geographies
-##############
-# This is the guide used http://eglenn.scripts.mit.edu/citystate/wp-content/uploads/2013/06/wpid-working_with_acs_R3.pdf
-
- only.chicago.ct <- population.df@geography[which(population.df@geography$tract %in% chicago.census.tracts@data$tractce10),]
- population.df@geography$comarea_n <- "NA"  
  
 
 
